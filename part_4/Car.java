aaf0dea28cddc3a6e3795a14b32b65d22c5c7219
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-09-13 15:07:01 +0900

    Add files via upload

---
+++ part_4/Car.java
@@ -0,0 +1,11 @@
+package part_4;
+
+public class Car {
+	
+	
+	// Java is an Object-Oriented Programming (OOP)
+	// OOP : creating objects that contain both data and methods.
+	// class : an object constructor, or a "blueprint" for creating objects. ex. 붕어빵 
+	
+
+}
---
+++ part_4/CarExam.java
@@ -0,0 +1,11 @@
+package part_4;
+
+public class CarExam {
+
+	public static void main(String[] args) {
+		Car c1 = new Car(); //instance
+		Car c2 = new Car();
+
+	}
+
+}

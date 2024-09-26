7e392d6f3a6742e127688a7931e2f820c433a518
Ann Kim <hellonaeunkim@gmail.com>
2024-09-26 21:05:53 +0900

    Update ConstructorExam.java

--- part_5/ConstructorExam.java
+++ part_5/ConstructorExam.java
@@ -6,7 +6,7 @@ public class ConstructorExam {
 
 	public static void main(String[] args) {
 		Constructor c1 = new Constructor(); // new Constructor(); -> 생성자(Constructor)
-		Constructor c2 = new Constructor("소방차");
+		Constructor c2 = new Constructor("소방차"); // class Constructor에서 기본 생성자 매개변수로 String값을 받음
 		Constructor c3 = new Constructor("구급차");
 		
 		System.out.println(c2.name);

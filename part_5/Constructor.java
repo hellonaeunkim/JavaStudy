0edb70dcf0842e2cae047a5c137bed3f9d9d0246
Ann Kim <hellonaeunkim@gmail.com>
2024-09-26 21:02:31 +0900

    Update Constructor.java

--- part_5/Constructor.java
+++ part_5/Constructor.java
@@ -1,13 +1,9 @@
 package part_5;
 
-import java.security.PublicKey;
-
 public class Constructor {
-
-	public static void main(String[] args) {
 		String name;
 		int number;
-	}
+
 	
 	public Constructor(String n) {
 		name = n;

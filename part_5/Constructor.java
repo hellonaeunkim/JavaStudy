360f52c4bfbe79bbfde43ce2121408fe5bca3e9b
Ann Kim <hellonaeunkim@gmail.com>
2024-09-27 21:15:14 +0900

    Update Constructor.java

--- part_5/Constructor.java
+++ part_5/Constructor.java
@@ -5,8 +5,8 @@ public class Constructor {
 		int number;
 
 	
-	public Constructor(String n) {
-		name = n;
+	public Constructor(String name) { // 변수명은 지나치게 축약하기 보다는 알아보기 쉽게 설정하는 것이 좋음 ex. n -> name
+		this.name = name; // 멤버 변수과 매개 변수 이름이 동일 할 때 사용
 	}
 
 }

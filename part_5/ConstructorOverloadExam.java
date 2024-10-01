0230c43bc842e5ca6eaf5259211fc814f8fc40e3
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-10-01 21:59:39 +0900

    Add files via upload

---
+++ part_5/ConstructorOverloadExam.java
@@ -0,0 +1,11 @@
+package part_5;
+
+public class ConstructorOverloadExam {
+	
+	public static void main(String[] args) {
+		ConstructorOverload c1 = new ConstructorOverload("test");
+		ConstructorOverload c2 = new ConstructorOverload();
+		ConstructorOverload c3 = new ConstructorOverload("구급차", 1234);
+	}
+	
+}

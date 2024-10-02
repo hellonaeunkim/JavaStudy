19d7ab6751db6af2161fecc17283a0a88004d89f
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-10-02 15:10:51 +0900

    Add files via upload
    Example of using Java Class

---
+++ com/eightcruz/packageexam/Hello.java
@@ -0,0 +1,8 @@
+package com.eightcruz.packageexam; // 관련된 class들을 폴더별로 관리할 때 사용되는 키워드
+
+public class Hello {
+
+}
+
+// package 이름은 대부분 도메인 이름을 거꾸로 적은 후 프로젝트 이름을 붙여 사용
+// package가 있는 class를 사용할 때는 import 키워드 사용

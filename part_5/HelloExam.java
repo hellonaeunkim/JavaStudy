75607c8c906c0de77942acbf4b7e03ad4b98627b
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-10-02 15:12:45 +0900

    Add files via upload
    Example of using package import

---
+++ part_5/HelloExam.java
@@ -0,0 +1,14 @@
+package part_5;
+
+import com.eightcruz.packageexam.Hello; // import : 다른 package의 class를 사용하기 위한 키워드 
+// import com.eightcruz.packageexam.*; -> 해당 package에 있는 모든 class 참
+
+public class HelloExam {
+
+	public static void main(String[] args) {
+		
+		Hello hello = new Hello(); // import 단축키 : command + shift + o
+
+	}
+
+}

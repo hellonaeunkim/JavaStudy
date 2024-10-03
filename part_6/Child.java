d4d639e91a925d1bda2ceed2455d6687e44cc993
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-10-03 19:28:21 +0900

    Add files via upload

---
+++ part_6/Child.java
@@ -0,0 +1,13 @@
+package part_6;
+
+public class Child extends Parent { // public class 클래스명 extends 부모클래스명
+	
+	// inheritance (상속) : 부모 클래스의 기능을 똑같이 사용가능 한 것 
+	// 부모 클래스 -> 자식 클래스로 상속 가능 but, 자식 클래스 -> 부모 클래스 사용 불가
+	
+	public void ppangpang() {
+		
+		System.out.println("빵빵");
+	}
+
+}

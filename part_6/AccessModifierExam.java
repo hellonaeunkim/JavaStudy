0fc15c4a646165327ac05de1f9c694b93b21b905
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-10-04 22:14:30 +0900

    Add files via upload

---
+++ part_6/AccessModifierExam.java
@@ -0,0 +1,19 @@
+package part_6;
+
+public class AccessModifierExam {
+
+	public static void main(String[] args) {
+		
+		AccessModifier obj = new AccessModifier();
+		
+		System.out.println(obj.p);
+		
+		System.err.println(obj.p2);
+		
+		System.err.println(obj.k);
+		
+		System.out.println(obj.i);
+
+	}
+
+}

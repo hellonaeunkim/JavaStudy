a1111444ca94799f96c16c9da06ab8e8c8a30def
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-10-08 01:53:56 +0900

    Add files via upload

---
+++ part_6/SuperChild.java
@@ -0,0 +1,15 @@
+package part_6;
+
+public class SuperChild extends SuperKeyword {
+	
+	public SuperChild() {
+		
+		super(); // refers to superclass (parent) objects.
+		
+//		super("Truck"); // If the Default Constructor need String value.
+		
+		System.out.println("This is SuperChild's Default Constructor.");
+		
+	}
+
+}

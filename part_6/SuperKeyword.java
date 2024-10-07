9882576847fb1ee3b1d73ccaae82b8ca659c2f79
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-10-08 01:52:19 +0900

    Add files via upload

---
+++ part_6/SuperKeyword.java
@@ -0,0 +1,15 @@
+package part_6;
+
+public class SuperKeyword {
+	
+	public SuperKeyword() {
+		
+		System.out.println("This is Super's Default Constructor.");
+	}
+	
+	public SuperKeyword(String name) {
+		
+		System.out.println("This is SuperKeyword's name Constructor.");
+	}
+
+}

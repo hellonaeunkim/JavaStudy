a34866b4bb4fb7fc85304d44fb9070c47974bc13
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-10-08 01:54:13 +0900

    Add files via upload

---
+++ part_6/SuperChildExam.java
@@ -0,0 +1,13 @@
+package part_6;
+
+public class SuperChildExam {
+
+	public static void main(String[] args) {
+		
+		SuperChild sc = new SuperChild();
+		
+//		SuperKeyword sk = new SuperKeyword();
+
+	}
+
+}

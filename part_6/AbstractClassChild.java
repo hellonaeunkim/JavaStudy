de0a639e732ebecf9a45b34039ac5fd75feedee8
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-10-07 16:15:56 +0900

    Add files via upload

---
+++ part_6/AbstractClassChild.java
@@ -0,0 +1,10 @@
+package part_6;
+
+public class AbstractClassChild extends AbstractClass {
+
+	@Override
+	public void abstractmethodA() {
+		System.out.println("Defining the abstract method of the parent class in the inherited child class.");
+	}
+
+}

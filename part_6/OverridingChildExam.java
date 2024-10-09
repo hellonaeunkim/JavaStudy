46041be4d407afa21238d681ec5158008c36c2f5
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-10-09 09:37:03 +0900

    Add files via upload

---
+++ part_6/OverridingChildExam.java
@@ -0,0 +1,13 @@
+package part_6;
+
+public class OverridingChildExam {
+
+	public static void main(String[] args) {
+		
+		OverridingChild oc = new OverridingChild();
+		
+		oc.overriding();
+
+	}
+
+}

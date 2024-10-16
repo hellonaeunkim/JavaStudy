ce81033e0eebfdb6c7e5737790c02835cb8bf55c
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-10-16 16:29:03 +0900

    Add files via upload
    Abstract class for creating anonymous class.

---
+++ part_7/AnonymousClass.java
@@ -0,0 +1,16 @@
+package part_7;
+
+public abstract class AnonymousClass {
+	
+	public abstract void exec();
+
+}
+
+/*
+ 
+Anonymous Class
+
+1.  inner classes with no name.
+2.  can be useful overriding methods of a class or interface
+
+ */

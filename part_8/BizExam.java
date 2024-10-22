4bf7b55b376b90f7e53460d31f721b58420e01a9
Ann Kim <121096778+hellonaeunkim@users.noreply.github.com>
2024-10-22 17:01:46 +0900

    Add files via upload
    Examples of handling exceptions.

---
+++ part_8/BizExam.java
@@ -0,0 +1,19 @@
+package part_8;
+
+public class BizExam {
+
+	public static void main(String[] args) {
+		
+		BizService biz = new BizService();
+		
+		biz.bizMethod(5);
+		try {
+			biz.bizMethod(-3);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
